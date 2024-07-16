9207568d8fb369460cf26ee31c9b6167a39ab93e
rhashimoto <ryuta.hashimoto@woven-planet.global>
2024-07-16 16:21:43 +0900

    update main

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ import pandas as pd
 import sqlite3
 from sqlite3 import Error
 
+# Local実行モード（サーバーからAPIでデータ取得せず，Local上のDumpからデータ取得する．API呼び出し回数を消費しないためのモード）
+IS_LOCAL_MODE = False
+
 # APIキー
 openweather_api_key = '71a673e4a77aa9bdc2e8a53467d75a82'
 news_api_key = '5d07dbd3bf274e6dae1030b52f4b8f1a'
-
-news_url = f"https://newsapi.org/v2/top-headlines?country=jp&apiKey={news_api_key}"
 
 cities = []
 cities.append({"jp":"東京", "en":"Tokyo"})
 cities.append({"jp":"京都", "en":"Kyoto"})
 cities.append({"jp":"大阪", "en":"Osaka"})
 cities.append({"jp":"福岡", "en":"Fukuoka"})
+cities.append({"jp":"札幌", "en":"Sapporo"})
 
 def add_city_id():
     for i, elem in enumerate(cities):
@@ -25,18 +27,19 @@ def main():
     add_city_id()
 
     # データを取得し，DataFrameに変換
-    weather_data = fetch_data.WeatherData(openweather_api_key)
+    weather_data = fetch_data.WeatherData(openweather_api_key, IS_LOCAL_MODE)
     weather_df = weather_data.make_df(cities)
     print("------ debug : weather_df ------")
     print(weather_df)
 
-    news_data = fetch_data.NewsData(news_api_key)
-    news_df = news_data.make_df(cities)
-    print("------ debug : news_df ------")
-    print(news_df)
+    # news_data = fetch_data.NewsData(news_api_key, IS_LOCAL_MODE)
+    # news_df = news_data.make_df(cities)
+    # print("------ debug : news_df ------")
+    # print(news_df)
 
     # SQLデータベースに統合
     # TBD．別の.pyファイルに分割しで計算する．
+    # 長越さん
 
     # 表示
     # TBD．別の.pyファイルに分割しで計算する．
