b6e03254e1b7926774cd0a9cadc4a7c615d871dc
rhashimoto <ryuta.hashimoto@woven-planet.global>
2024-07-16 18:10:44 +0900

    コメント等の調整

--- fetch_data.py
+++ fetch_data.py
@@ -56,6 +56,10 @@ class Data(ABC):
             pass
         return
 
+    # @abstractmethod
+    def print_df_for_debug(self, df):
+        pass
+
 #________________________________________________________________________________________________________________________
 class WeatherData(Data):
     def __init__(self, API_key, is_local_mode):
@@ -78,16 +82,21 @@ class WeatherData(Data):
             city_name = city["en"]
             data = self._get_data(city_name)
             dict.append({
-                'city_id'       : city["city_id"],
-                'weather_id'    : i,
-                'temperature'   : data['main']['temp']-273.15,
-                'weather'       : data['weather'][0]['description'],
-                '(DEBUG)city_name'     : city_name,                # for debug
-                '(DEBUG)lat'           : data['coord']['lat'],     # for debug
-                '(DEBUG)lon'           : data['coord']['lon']     # for debug
+                'city_id'     : city["city_id"],
+                'weather_id'  : i,
+                'temperature' : data['main']['temp']-273.15,
+                'weather'     : data['weather'][0]['description'],
+                '(DEBUG)city_name' : city_name,                # for debug
+                '(DEBUG)lat'       : data['coord']['lat'],     # for debug
+                '(DEBUG)lon'       : data['coord']['lon']     # for debug
             })
         df = pd.DataFrame(dict)
         return(df)
+
+    def print_df_for_debug(self, df):    
+        print("\n######################## debug : weather_df ########################")
+        print(df)
+        return
 
 #________________________________________________________________________________________________________________________
 class NewsData(Data):
@@ -98,7 +107,7 @@ class NewsData(Data):
         news_url = 'https://newsapi.org/v2/everything'
         params = {
             'q': city_name,
-            'domains' : 'nhk.or.jp',
+            'domains' : 'nhk.or.jp', # 対象記事はNHKに限定する．
             'sortBy'  : 'publishedAt',
             'apiKey'  : self.API_key
         }
@@ -106,6 +115,22 @@ class NewsData(Data):
 
     def _get_json_file_name(self, city_name):
         return f'news_{city_name}'
+
+    def __extract_news(self, data, city_name):
+        max_news_num = 3
+        list = []
+        for elem in data["articles"]:
+            if city_name in elem["title"]:
+                abstract = f'【{elem["publishedAt"][:10]}】{elem["title"]}'
+                list.append(abstract)
+                if len(list) >= max_news_num:
+                    return list
+
+        # もしも十分数記事が取得できなかった場合に，配列外アクセスを防ぐために空の要素を加える
+        for i in range(max_news_num - len(list)):
+            abstract = ""
+            list.append(abstract)
+        return list
     
     def make_df(self, cities):
         dict = []
@@ -114,28 +139,23 @@ class NewsData(Data):
             data = self._get_data(city_name)
             news_list = self.__extract_news(data, city_name)
             dict.append({
-                'city_id'       : city["city_id"],
-                'news_id'       : i,
-                'news_1'        : news_list[0],
-                'news_2'        : news_list[1],
-                'news_3'        : news_list[2],
-                '(DEBUG)city_name'     : city_name
+                'city_id' : city["city_id"],
+                'news_id' : i,
+                'news_1'  : news_list[0],
+                'news_2'  : news_list[1],
+                'news_3'  : news_list[2],
+                '(DEBUG)city_name' : city_name
             })
         df = pd.DataFrame(dict)
         return(df)
 
-    def __extract_news(self, data, city_name):
-        max_news_num = 3
-        list = []
-        for elem in data["articles"]:
-            if city_name in elem["title"]:
-                abstract = f'【{elem["publishedAt"][:10]}】【{elem["source"]["name"]}】{elem["title"]}'
-                list.append(abstract)
-                if len(list) >= max_news_num:
-                    return list
-
-        for i in range(max_news_num - len(list)):
-            abstract = ""
-            list.append(abstract)
-        return list
+    def print_df_for_debug(self, df):   
+        print("\n######################## debug : news_df ########################")
+        for i in range(len(df)):
+            elem = df.iloc[i]
+            print(f'city_id = {elem.loc["city_id"]}, news_id = {elem.loc["news_id"]}, (DEBUG)city_name = {elem.loc["(DEBUG)city_name"]}')
+            print(f' - news_1 : {elem.loc["news_1"]}')
+            print(f' - news_2 : {elem.loc["news_2"]}')
+            print(f' - news_3 : {elem.loc["news_3"]}\n')
+        return
 
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import fetch_data
 import pandas as pd
 import sqlite3
-from sqlite3 import Error
 
+#________________________________________________________________________________________________________________________
 # Local実行モード（サーバーからAPIでデータ取得せず，Local上のDumpからデータ取得する．API呼び出し回数を消費しないためのモード）
 IS_LOCAL_MODE = True
+# デバッグ用のprintを表示するモード
+IS_DEBUG_PRINT_MODE = True
 
+#________________________________________________________________________________________________________________________
 # APIキー
 openweather_api_key = '71a673e4a77aa9bdc2e8a53467d75a82'
 news_api_key = '5d07dbd3bf274e6dae1030b52f4b8f1a'
 
+#________________________________________________________________________________________________________________________
+# 対象都市
 cities = []
 cities.append({"jp":"東京", "en":"Tokyo"})
 cities.append({"jp":"京都", "en":"Kyoto"})
@@ -17,31 +22,48 @@ cities.append({"jp":"大阪", "en":"Osaka"})
 cities.append({"jp":"福岡", "en":"Fukuoka"})
 cities.append({"jp":"札幌", "en":"Sapporo"})
 
+#________________________________________________________________________________________________________________________
 def add_city_id():
+    """
+    city_idは都市ごとにuniqueであれば何でも良いので、citiesのindexをIDとして用いる。
+    IDが自動で採番されることと合わせて、これらによりIDの唯一性を担保する。
+    """
     for i, elem in enumerate(cities):
         elem["city_id"] = i
     return
-
-
+    
 def main():
-    add_city_id()
+    # \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ 
+    # 【1】データを取得し，DataFrameに変換
+    # \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ 
 
-    # データを取得し，DataFrameに変換
+    # 「city_name」に相当するDataFrame
+    add_city_id()
+    city_df =  pd.DataFrame(cities)
+
+    # 「weather_current」に相当するDataFrame
     weather_data = fetch_data.WeatherData(openweather_api_key, IS_LOCAL_MODE)
     weather_df = weather_data.make_df(cities)
-    print("------ debug : weather_df ------")
-    print(weather_df)
+    if IS_DEBUG_PRINT_MODE:
+        weather_data.print_df_for_debug(weather_df)
 
+    # 「news_latest」に相当するDataFrame
     news_data = fetch_data.NewsData(news_api_key, IS_LOCAL_MODE)
     news_df = news_data.make_df(cities)
-    print("------ debug : news_df ------")
-    print(news_df)
+    if IS_DEBUG_PRINT_MODE:
+        news_data.print_df_for_debug(news_df)
 
-    # SQLデータベースに統合
+    # \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ 
+    # 【2】SQLデータベースに統合
+    # \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ 
     # TBD．別の.pyファイルに分割しで計算する．
     # 長越さん
 
-    # 表示
+    # \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ 
+    # 【3】表示
+    # \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ 
     # TBD．別の.pyファイルに分割しで計算する．
+
+#________________________________________________________________________________________________________________________
 if __name__ == "__main__":
     main()
